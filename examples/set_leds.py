725914d39fb63ebd4769be4863d86b770e2c6cef
Juan Gallostra <juangallostra@gmail.com>
2019-02-25 11:50:45 +0100

    Add LEDs example

---
+++ examples/set_leds.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Author: Juan Gallostra, jgallostra<at>bonadrone.com
+# Date: 1-4-2019
+
+import sys
+# Add parent folder to path so that the API can be imported.
+# This shouldn't be required if the package has been installed via pip
+sys.path.insert(0, '../')
+sys.path.insert(0, '../mosquito/')
+
+import time
+from mosquito import mapi
+
+def main():
+	"""
+	Function that gets executed when the script is directly
+	called from the command line.
+
+	What it does is set on and off the LEDs of the board at
+	2 seconds intervals
+	"""
+
+	Mosquito = mapi.Mosquito()
+	Mosquito.connect()
+
+	# turn on and off the LEDs of the board at 2 second intervals
+	# Note how, since the method expects keyword arguments, the order
+	# in which they are passed doesn't matter. If any of the LEDs is
+	# omitted its current status is preserved
+	Mosquito.set_leds(red=0, green=0, blue=0)
+	Mosquito.set_leds(blue=1) # The status of the other LEDs is preserved
+	time.sleep(2)
+	Mosquito.set_leds(blue=0)
+	Mosquito.set_leds(red=1)
+	time.sleep(2)
+	Mosquito.set_leds(red=0)
+	Mosquito.set_leds(green=1)
+	time.sleep(2)
+	Mosquito.set_leds(green=0)
+	Mosquito.set_leds(blue=1, red=1, green=1)
+	time.sleep(2)
+
+	# Disconnect from the mosquito when finished
+	Mosquito.disconnect()
+
+if __name__ == "__main__":
+	main()
