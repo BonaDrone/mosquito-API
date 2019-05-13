e3146946db96fbe6a3b25c7a7aaf47d7f51fe530
Juan Gallostra <juangallostra@gmail.com>
2019-05-13 12:14:07 +0200

    Update comments

--- examples/simple_flight.py
+++ examples/simple_flight.py
@@ -19,7 +19,7 @@ def main():
 	called from the command line.
 
 	This scripts makes the Mosquito perform a simple flight that
-	consists in taking off at 30 cm, hovering for 2 seconds and landing.
+	consists in taking off at 100 cm, hovering for 2 seconds and landing.
 	"""
 	Mosquito = mapi.Mosquito()
 	Mosquito.connect()
@@ -32,9 +32,9 @@ def main():
 	# Take off at 100 cm, hover for 2 seconds and land
 	Mosquito.arm()
 	time.sleep(0.5)
-	Mosquito.take_off(100)
-	Mosquito.hover(2)
-	Mosquito.land()
+	Mosquito.take_off(100) # height is in cm
+	Mosquito.hover(2) # hover duration is in seconds
+	Mosquito.land() # land doesn't take any parameters
 	time.sleep(0.5)
 	Mosquito.disarm()
 
